a627e4d7244f9fa68c1f6616b0d05c371eba18cd
Javier Razo <javier.razo1221@gmail.com>
2022-10-21 07:28:00 -0700

    warmup2

--- js/warmup.js
+++ js/warmup.js
@@ -12,3 +12,22 @@ let potatoes = {
 };
 
 console.log(getPrice(potatoes));
+
+// Write a function that when passed an array will return the longest string from the array.
+//     Ex. const arr = [“Green”, “Blue”, “Periwinkle”];
+//getLongestString(arr) // Returns “Periwinkle”
+function getLongestString(arr) {
+    let longestStr = "";
+    for (let str of arr) {
+        if(str.length > longestStr.length){
+            longestStr = str;
+        }
+    }
+    return longestStr;
+}
+const colors = ["Green", "Blue", "Periwinkle"];
+console.log(getPrice(colors));
+
+
+
+
