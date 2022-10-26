de2b212067b8775e290dc5faff8ce8a679c1d1a2
Javier Razo <javier.razo1221@gmail.com>
2022-10-26 07:45:26 -0700

    warmup3

--- js/warmup.js
+++ js/warmup.js
@@ -27,7 +27,51 @@ function getLongestString(arr) {
 }
 const colors = ["Green", "Blue", "Periwinkle"];
 console.log(getPrice(colors));
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
 
+//getTallestHamster(hamsters); // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
 
+function getTallestHamster(arr){
+    let tallest= {heightInMM: 0};
+    arr.forEach(function(el){
+       if (el.heightInMM > tallest.heightInMM){
+           tallest = el;
+       }
+    });
+    return tallest;
+}
+console.log(getTallestHamster(hamsters));
 
 
