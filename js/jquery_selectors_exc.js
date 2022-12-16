2f6d4917d2159268de1ccfa9cb986c311f9d0299
Javier Razo <javier.razo1221@gmail.com>
2022-12-16 10:26:01 -0700

    added selectors

--- js/jquery_selectors_exc.js
+++ js/jquery_selectors_exc.js
@@ -1,13 +1,16 @@
 "use strict";
 
 $(function() {
-    $('#codeup');
-    var contents = $('#codeup').html();
-    alert(contents);
+    alert($("#codeup").html());
+     alert($("#main").html());
 
-    $("li").css("font-size", "20px");
+     $(".codeup").css("border", "12px solid blue");
 
+    $("li").css("font-size", "30px");
+     $("h1").css("background", "green");
+     $("p").css("background", "yellow");
+     alert($("h1").html());
+     console.log($("h1").html());
 
-
-    $("h1, p, li").css("background-color", "red");
+    $("h1, p, li").css("background-color", "blue");
 });
